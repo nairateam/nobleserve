541d691db86d7db6c12caff5b9ac6d2090399aa9
unknown <oghenefegor24@gmail.com>
2023-07-07 12:36:09 +0100

    updated

--- nobleserve/settings.py
+++ nobleserve/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['http://fegor.pythonanywhere.com']
+ALLOWED_HOSTS = ['Fegor.pythonanywhere.com']
 # ALLOWED_HOSTS = []
 
 # Application definition
