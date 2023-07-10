9c6a1118cd89884625723f62a856c09e49eefb4c
unknown <oghenefegor24@gmail.com>
2023-07-10 12:32:33 +0100

    updated

--- nobleserve/settings.py
+++ nobleserve/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['Fegor.pythonanywhere.com']
-#ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = ['Fegor.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -82,26 +82,26 @@ WSGI_APPLICATION = 'nobleserve.wsgi.application'
 # Database
 #https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': config('DB_NAME'),
-#         'USER': config('DB_USER'),
-#         'PASSWORD': config('DB_PASSWORD'),
-#         'HOST': config('DB_HOST'),
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Fegor$nobleserve',
-        'USER': 'Fegor',
-        'PASSWORD': 'quv@)BX*Cioo',
-        'HOST': 'Fegor.mysql.pythonanywhere-services.com',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'Fegor$nobleserve',
+#         'USER': 'Fegor',
+#         'PASSWORD': 'quv@)BX*Cioo',
+#         'HOST': 'Fegor.mysql.pythonanywhere-services.com',
+#     }
+# }
 
 #define it to use model.
 AUTH_USER_MODEL = 'accounts.User'
