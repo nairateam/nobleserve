e58c6ccc6bb10e6b6a7d57aaeced6d47587f5b0e
unknown <oghenefegor24@gmail.com>
2023-07-26 18:15:00 +0100

    updated

--- nobleserve/settings.py
+++ nobleserve/settings.py
@@ -170,7 +170,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
     'nobleserve/static',
 ]
@@ -180,7 +180,7 @@ STORAGES = {
     "default": {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
-    "staticfiles": {
+    "static": {
         "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
     },
 }
